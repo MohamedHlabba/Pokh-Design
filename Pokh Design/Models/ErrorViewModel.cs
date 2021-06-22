806a5862bc56b8982aca4c49e531caa12d3c84d0
MohamedHlabba <mohamed.hlabba@esprit.tn>
2021-06-22 20:05:08 +0200

    Add project files.

---
+++ Pokh Design/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pokh_Design.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
